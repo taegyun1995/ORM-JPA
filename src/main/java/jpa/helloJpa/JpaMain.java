ab4b149754b9fd4a46f130176d7435c4a448a706
gyunny <xorbs15923@naver.com>
2022-11-10 16:56:30 +0900

    ORM-JPA entity 매핑 기초

--- src/main/java/jpa/helloJpa/JpaMain.java
+++ src/main/java/jpa/helloJpa/JpaMain.java
@@ -4,7 +4,6 @@ import javax.persistence.EntityManager;
 import javax.persistence.EntityManagerFactory;
 import javax.persistence.EntityTransaction;
 import javax.persistence.Persistence;
-import java.util.List;
 
 public class JpaMain {
 
@@ -15,16 +14,14 @@ public class JpaMain {
         tx.begin();
 
         try {
-//            Member findMember = em.find(Member.class, 1L);
-            List<Member> result = em.createQuery("select m from Member as m", Member.class)
-                    .setFirstResult(5)
-                    .setMaxResults(8)
-                    .getResultList();
+            Member member = em.find(Member.class, 200L);
+            member.setName("AAAAA");
 
-            for (Member member : result) {
-                System.out.println("member.getName() = " + member.getName());
-            }
+            em.clear();
 
+            Member member2 = em.find(Member.class, 200L);
+
+            System.out.println("===============");
             tx.commit();
         } catch (Exception e) {
             tx.rollback();
--- src/main/java/jpa/helloJpa/Member.java
+++ src/main/java/jpa/helloJpa/Member.java
@@ -1,14 +1,20 @@
 package jpa.helloJpa;
 
+import javax.persistence.Column;
 import javax.persistence.Entity;
 import javax.persistence.Id;
+import javax.persistence.Table;
 
 @Entity
 public class Member {
 
     @Id
     private Long id;
+
+    @Column(unique = true, length = 10)
     private String name;
+
+    private int age;
 
     public Long getId() {
         return id;
@@ -24,5 +30,22 @@ public class Member {
 
     public void setName(String name) {
         this.name = name;
+    }
+
+    public int getAge() {
+        return age;
+    }
+
+    public void setAge(int age) {
+        this.age = age;
+    }
+
+    public Member() {
+    }
+
+    public Member(Long id, String name, int age) {
+        this.id = id;
+        this.name = name;
+        this.age = age;
     }
 }
