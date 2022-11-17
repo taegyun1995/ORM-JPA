1cee996734c60963daf2eda1ba6c39d3d330cf5d
gyunny <xorbs15923@naver.com>
2022-11-17 13:41:22 +0900

    ORM-JPA FetchType.Lazy/지연로딩 -> JPQL, 엔티티 그래프 필요

--- src/main/java/helloJpa/JpaMain.java
+++ src/main/java/helloJpa/JpaMain.java
@@ -4,6 +4,7 @@ import org.hibernate.Hibernate;
 
 import javax.persistence.*;
 import java.time.LocalDateTime;
+import java.util.List;
 
 public class JpaMain {
 
@@ -14,18 +15,34 @@ public class JpaMain {
         tx.begin();
 
         try {
+            Team team = new Team();
+            team.setName("A");
+            em.persist(team);
+
+            Team team2 = new Team();
+            team2.setName("B");
+            em.persist(team2);
 
             Member member1 = new Member();
             member1.setUsername("member1");
+            member1.setTeam(team);
             em.persist(member1);
+
+            Member member2 = new Member();
+            member2.setUsername("member2");
+            member2.setTeam(team2);
+            em.persist(member2);
 
             em.flush();
             em.clear();
 
-            Member refMember = em.getReference(Member.class, member1.getId());
-            System.out.println("refMember = " + refMember.getClass()); // Proxy
+//            Member m = em.find(Member.class, member1.getId());
 
-            Hibernate.initialize(refMember); // 강제 초기화
+            List<Member> members = em.createQuery("select m from Member m join fetch m.team", Member.class)
+                    .getResultList();
+
+            // SQL : select * from Member
+            // SQL : select * from Team where TEAM_ID = xxx
 
 
             tx.commit();
